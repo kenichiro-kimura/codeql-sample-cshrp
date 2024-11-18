8b3e95c8c43815b9284d2c5d187f113e39771bfc
木村 健一郎 <k-kimura@alterbooth.com>
2024-11-18 15:52:24 +0900

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace codeql_sample_cshrp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
